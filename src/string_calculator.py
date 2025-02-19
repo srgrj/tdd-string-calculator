bff5ec786c9fd2a9f198dcd94456951ac4c0d815
Somesh Garje <someshgarje@gmail.com>
2025-02-19 19:55:19 +0530

    Updating Readme

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -15,5 +15,15 @@ class StringCalculator:
             if new_delimiter:
                 self.register_delimiter(new_delimiter.group(1))
                 numbers = new_delimiter.group(2)
-            return sum(map(int, re.split("|".join(self._customer_delimiters), numbers)))
+            number_list = list(map(int, re.split("|".join(self._customer_delimiters), numbers)))
+            negatives = [str(x) for x in number_list if x < 0]
+            if not negatives:
+                return sum(number_list)
+            raise ValueError(f"Found negative numbers: {','.join(negatives)}")
         return 0
+
+
+if __name__ == '__main__':
+    numbers = str(input("Provide number string: "))
+    string_calculator = StringCalculator()
+    print("Sum of numbers is: ", string_calculator.add(numbers))
