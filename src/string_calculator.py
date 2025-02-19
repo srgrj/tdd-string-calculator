cc00584f7caf2146b3368d9ce2d27381fae910d3
Somesh Garje <someshgarje@gmail.com>
2025-02-19 18:22:49 +0530

    Major refactor to accept custom delimiter

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -1,4 +1,14 @@
-def add(numbers: str) -> int:
-    if numbers:
-        return sum(map(int, numbers.split(',')))
-    return 0
+import re
+
+
+class StringCalculator:
+    def __init__(self):
+        self._customer_delimiters = [',']
+
+    def register_delimiter(self, delimiter):
+        self._customer_delimiters.append(delimiter)
+
+    def add(self, numbers: str) -> int:
+        if numbers:
+            return sum(map(int, re.split("|".join(self._customer_delimiters), numbers)))
+        return 0
--- src/tests/test_string_calculator.py
+++ src/tests/test_string_calculator.py
@@ -1,21 +1,24 @@
-from src.string_calculator import add
+from src.string_calculator import StringCalculator
+
+string_calculator = StringCalculator()
 
 
 def test_empty_string():
-    assert add("") == 0
+    assert string_calculator.add("") == 0
 
 
 def test_single_number():
-    assert add('1') == 1
+    assert string_calculator.add('1') == 1
 
 
 def test_comma_separated_two_numbers():
-    assert add('1,2') == 3
+    assert string_calculator.add('1,2') == 3
 
 
 def test_multiple_numbers():
-    assert add('1,2,3,4,5') == 15
+    assert string_calculator.add('1,2,3,4,5') == 15
 
 
 def test_custom_delimiter():
-    assert add("1\n2,3") == 6
+    string_calculator.register_delimiter("\n")
+    assert string_calculator.add("1\n2,3") == 6
